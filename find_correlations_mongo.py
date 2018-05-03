bc8040ec1691ecb6bf0aa310191d465e81fd0fcb
Krzysztof Pawlikowski <kris@whiteeaglecs.com>
2018-05-02 22:12:40 -0400

    final

--- find_correlations_mongo.py
+++ find_correlations_mongo.py
@@ -2,13 +2,14 @@ from pymongo import MongoClient
 import os
 import pandas
 import ntpath
+import re
 
 
 # Step 1 : Get a list of all the collections
 
 # Step 2 : Filter out the list to contain only the collections which start with dataset_
 
-# Step 3 : Create a new Document which will hold our YEAR objects.
+# Step 3 : Create a new Collection which will hold our YEAR objects.
 
 # Step 4: Each YEAR object will hold individual data points for that year from all data sources.
 
@@ -16,12 +17,13 @@ class DataAggregatorMongo:
 
     def __init__(self):
         self.document_prefix = "dataset_"
+        self.document_prefix_regex = r'^dataset_.*'
         self.app_database = "myflaskapp"
         self.database_user = "flask"
         self.database_pw = "password"
         self.meta_data_document = "data_sources_meta_data"
         self.correlations_document = "correlations"
-        self.aggregate_document = "aggregate_of_data"
+        self.aggregate_collection = "aggregate_of_data_sets"
 
         self.mongo_db_server = 'pymongo_test'
 
@@ -33,26 +35,91 @@ class DataAggregatorMongo:
         print("connect_to_mongo() ran")
         return conn
 
-    def get_list_of_collections_starting_with(self,start_with):
+    def get_list_of_dataset_collections(self):
+        # Connect to Mongo
+        client = self.connect_to_mongo()
 
-        conn = self.connect_to_mongo()
+        # Specify the database
+        db = client['{}'.format(self.app_database)]
 
-        server_name = self.mongo_db_server
+        # Get all the collections in the database
+        collections = db.list_collection_names()
 
-        db = conn['{}'.format(server_name)]
+        # Compile the regex
+        r = re.compile(self.document_prefix_regex)
 
-        flask_database = db['{}'.format(self.app_database)]
+        # Filter out only collections which start with dataset_
+        data_set_collections = [x for x in collections if r.match(x)]
 
-        print(flask_database)
+        # return the list
+        return data_set_collections
 
-        collections = flask_database.getCollectionNames()
+    def create_data_aggregation_collection(self):
+        client = self.connect_to_mongo()
 
-        print(collections)
+        # Specify the database
+        db = client['{}'.format(self.app_database)]
+
+        # Create the collection if it does not exist
+        if self.aggregate_collection not in db.list_collection_names():
+            db.create_collection('{}'.format(self.aggregate_collection))
+
+    def insert_into_aggregate_collection(self):
+        client = self.connect_to_mongo()
+
+        # Specify the database
+        db = client['{}'.format(self.app_database)]
+
+        dataset_collection = db.get_collection('dataset_gasoline_retail_prices_new_york_city_new_york')
+
+        cursor = dataset_collection.find({})
+        for document in cursor:
+            self.create_year_document(document['YEAR'])
+            self.insert_data_point_into_year_document(document)
+
+    def create_year_document(self, year):
+        # Connect to Mongo
+        client = self.connect_to_mongo()
+
+        # Specify the database
+        db = client['{}'.format(self.app_database)]
+
+        # Create the document to be inserted. Note that YEAR will be a string. We may need to change this later
+        doc = {'YEAR': year, 'DATA_POINTS': []}
+
+        # Check to see if a YEAR document exists. If it exists we do not create it again.
+        count = db['{}'.format(self.aggregate_collection)].count({"YEAR": year})
+
+        if count == 0:
+            db['{}'.format(self.aggregate_collection)].insert(doc)
+
+    def insert_data_point_into_year_document(self, document):
+        # Connect to Mongo
+        client = self.connect_to_mongo()
+
+        # Specify the database
+        db = client['{}'.format(self.app_database)]
+
+        re.compile(r'^VAL_.*')
+
+        doc = {}
+
+        # Create the data point to be inserted
+        for key, value in document.items():
+            if re.match(key):
+                doc = {key: value}
+
+        
 
 
 
 di = DataAggregatorMongo()
 
-di.connect_to_mongo()
+dataset_collections = di.get_list_of_dataset_collections()
 
-di.get_list_of_collections_starting_with("test")
+di.create_data_aggregation_collection()
+
+# for dataset in dataset_collections:
+#   print(dataset)
+
+di.insert_into_aggregate_collection()
