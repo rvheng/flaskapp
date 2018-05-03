2f6c4650184a4e7262195e84baa1ad90c11b7ad1
Krzysztof Pawlikowski <kris@whiteeaglecs.com>
2018-05-03 00:57:15 -0400

    final commit

--- backend_data_import_mongo.py
+++ backend_data_import_mongo.py
@@ -1,9 +1,14 @@
 from data_import_scripts import data_import_mongo
+import time
+
+start = time.time()
 
 di = data_import_mongo.DataImportMongo()
-
-#di.test()
 
 di.import_csvs("asm")
 
 di.import_csvs("gasoline_prices")
+
+end = time.time()
+
+print(end - start)
